e8c857065ec80cd9d651a86546ae7e84e5e3e335
luver <arnfourm@gmail.com>
2025-06-18 12:43:33 +0300

    groupGRAD done

--- todolistAltenar/todolistProject/todolistProject.dataAccess/CRUD/GroupCRUD.cs
+++ todolistAltenar/todolistProject/todolistProject.dataAccess/CRUD/GroupCRUD.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using todolistProject.Core.Models;
 using todolistProject.dataAccess.Entities;
 
@@ -29,6 +30,49 @@ namespace todolistProject.dataAccess.CRUD
             return groupListReturn;
         }
 
+        public async Task<int> CreateGroup(Group group)
+        {
+            var userEntity = await _dbContext.Users.FindAsync(group.user.idUser);
+            if (userEntity == null)
+            {
+                throw new Exception($"User not found. ID: {group.user.idUser}");
+            }
 
+            var groupEntity = new GroupEntity
+            {
+                idGroup = group.idGroup,
+                userID = group.user.idUser,
+                titleGroup = group.titleGroup,
+                User = userEntity
+            };
+
+            await _dbContext.Groups.AddAsync(groupEntity);
+            await _dbContext.SaveChangesAsync();
+
+            return groupEntity.idGroup;
+        }
+
+        public async Task<int> UpdateGroup(int idGroup, string titleGroup)
+        {
+            await _dbContext.Groups
+                .Where(group => group.idGroup == idGroup)
+                .ExecuteUpdateAsync(update => update
+                    .SetProperty(group => group.titleGroup, group => titleGroup);
+
+            await _dbContext.SaveChangesAsync();
+
+            return idGroup;
+        }
+
+        public async Task<int> DeleteGroup(int idGroup)
+        {
+            await _dbContext.Groups
+                .Where(group => group.idGroup == idGroup)
+                .ExecuteDeleteAsync();
+
+            await _dbContext.SaveChangesAsync();
+
+            return idGroup;
+        }
     }
 }
