0ea0982aaf7a0ed2e75ab92f25bb067ce6163787
luver <arnfourm@gmail.com>
2025-06-19 18:33:44 +0300

    some UI changed

--- todolistAltenar/frontend/javascript/mainNoteAction.js
+++ todolistAltenar/frontend/javascript/mainNoteAction.js
@@ -7,15 +7,37 @@ async function fetchNote() {
         const responseGroups = await fetch(`http://localhost:5140/Group/${userId}/GetGroupByUserId`);
         const allGroups = await responseGroups.json();
 
-        const responseNotes = await fetch('http://localhost:5140/Note');
-        const allNotes = await responseNotes.json();
+        const responseNotes = await fetch(`http://localhost:5140/Note/${userId}/GetNoteByUserId`);
+        let allNotes = await responseNotes.json();
 
         const notesContainer = document.getElementById('noteContainer');
 
         allGroups.forEach(singleGroup => {
             const groupShow = document.createElement('div');
             groupShow.classList.add('groupDiv');
+
+            const groupNameShow = document.createElement('p');
+            groupNameShow.textContent = "⇩ " + singleGroup.titleGroup;
+            groupNameShow.classList.add('groupName');
+
             notesContainer.appendChild(groupShow);
+            groupShow.appendChild(groupNameShow);
+
+            allNotes.forEach(singleNote => {
+                if (singleNote.groupID === singleGroup.idGroup){
+                    allNotes = allNotes.filter(note => note != singleNote);
+
+                    const noteShow = document.createElement('div');
+                    noteShow.classList.add('noteDiv');
+                    
+                    const noteNameShow = document.createElement('p');
+                    noteNameShow.textContent = singleNote.titleNote;
+                    noteNameShow.classList.add('noteName')
+
+                    groupShow.appendChild(noteShow);
+                    noteShow.appendChild(noteNameShow);
+                }
+            });
         });
     } catch (error) {
         console.error('ошибка', error);
