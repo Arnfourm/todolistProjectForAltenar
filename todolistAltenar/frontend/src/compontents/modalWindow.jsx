d9c515215b7d399857c9d9d34a450a09fe10f81d
luver <arnfourm@gmail.com>
2025-06-24 19:10:17 +0300

    modalWindow DONE

--- todolistAltenar/frontend/src/compontents/modalWindow.jsx
+++ todolistAltenar/frontend/src/compontents/modalWindow.jsx
@@ -3,17 +3,54 @@ import axios from "axios";
 import '../styles/modalWindow.css'
 
 function ModalWindow({ isOpen, onClose, note, content }) {
-    if (!isOpen || !note || !content) return null;
+    const [titleNote, SetTitle] = useState('');
+    const [contentNote, SetContent] = useState('');
+
+    useEffect(() => {
+        if (note && content) {
+            SetTitle(note.titleNote);
+            SetContent(content.noteContent);
+        }
+    }, [note, content]);
+
+    if (!isOpen) return null;
 
     return (
         <div className="modal">
             <div className="modalContent">
                 <span className="close" onClick={onClose}>&times;</span>
-                <input type="text" className="nameNoteInput" defaultValue={note.titleNote} />
-                <textarea className="editInput" defaultValue={content.noteContent}></textarea>
+                <input type="text" className="nameNoteInput" value={titleNote} onChange={(e) => SetTitle(e.target.value)} />
+                <textarea className="editInput" value={contentNote} onChange={(e) => SetContent(e.target.value)} />
                 <section className="modalButtons">
-                    <button>Отмена</button>
-                    <button>Сохранить</button>
+                    <button onClick={() => {
+                        SetTitle(note.titleNote);
+                        SetContent(content.noteContent);
+                    }}>Отмена</button>
+                    <button onClick={() => {
+                        if (note.titleNote !== titleNote) {
+                            const newTitleRequest = {
+                                userID: note.userID,
+                                titleNote: titleNote,
+                                groupID: note.groupID
+                            }
+                            axios.put(`http://localhost:5140/Note/${note.noteID}`, newTitleRequest)
+                                .then(() => {
+                                    note.titleNote = titleNote;
+                                })
+                                .catch(error => ("Не удалось изменить название", error));
+                        };
+
+                        if (content.noteContent !== contentNote) {
+                            const newContentRequest = {
+                                noteContent: contentNote
+                            };
+                            axios.put(`http://localhost:5140/Note/Content/${note.noteID}`, newContentRequest)
+                                .then(() => {
+                                    content.noteContent = contentNote;
+                                })
+                                .catch(error => ("Не удалось изменить контент", error));
+                        };
+                    }}>Сохранить</button>
                 </section>
             </div>
         </div>
