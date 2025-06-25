71b50ff5ff51246b61f82351d82c3b7516e2ae66
luver <arnfourm@gmail.com>
2025-06-26 01:23:50 +0300

    added dockerfile for frontend + docker compose

--- todolistAltenar/backend/todolistProject.API/Program.cs
+++ todolistAltenar/backend/todolistProject.API/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddScoped<IUserCRUD, UserCRUD>();
 builder.Services.AddScoped<IGroupService, GroupService>();
 builder.Services.AddScoped<IGroupCRUD, GroupCRUD>();
 
+// ask about dockerfile mapping
 builder.WebHost.UseUrls("http://0.0.0.0:5140");
 
 var app = builder.Build();
