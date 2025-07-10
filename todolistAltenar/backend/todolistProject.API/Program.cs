17e3ee4a77a08556996122ea826a737daba1d279
luver <arnfourm@gmail.com>
2025-07-10 19:18:35 +0300

    trying

--- todolistAltenar/backend/todolistProject.API/Program.cs
+++ todolistAltenar/backend/todolistProject.API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Servasdices.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<todolistDbContext>(
