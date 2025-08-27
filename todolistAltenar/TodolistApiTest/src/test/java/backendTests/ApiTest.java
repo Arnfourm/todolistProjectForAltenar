ed033899a4748b64fb3e9d8e0b1ffca66dbdcf97
Luverk1d <57453642+Arnfourm@users.noreply.github.com>
2025-08-28 00:31:32 +0300

    Test change

--- todolistAltenar/TodolistApiTest/src/test/java/backendTests/ApiTest.java
+++ todolistAltenar/TodolistApiTest/src/test/java/backendTests/ApiTest.java
@@ -37,7 +37,7 @@ public class ApiTest {
         String newUser = String.format("""
                 {
                   "username": "%s",
-                  "userEmail": "%s",
+                  "usermEmail": "%s",
                   "userPassword": "%s"
                 }
                 """, newUserName, newEmailAddress, newPassword);
